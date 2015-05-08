9a6c5adba3f7f41d7f388295fbbc86cd652d669f
Tevic <tevic.tt@gmail.com>
2015-05-08 18:28:56 +0800

    Update CPU Governor

--- System/Performance/Interactive2/changepowermode.sh
+++ System/Performance/Interactive2/changepowermode.sh
@@ -11,11 +11,11 @@ case "$target" in
                  echo 2265600                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                 echo 20000                                > /sys/devices/system/cpu/cpufreq/conservative/above_hispeed_delay
-                 echo 60                                   > /sys/devices/system/cpu/cpufreq/conservative/go_hispeed_load
-                 echo 1190400                              > /sys/devices/system/cpu/cpufreq/conservative/hispeed_freq
-                 echo 70                                   > /sys/devices/system/cpu/cpufreq/conservative/target_loads
-                 echo 40000                                > /sys/devices/system/cpu/cpufreq/conservative/min_sample_time
+                 echo 20000                                > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                 echo 60                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                 echo 1190400                              > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                 echo 70                                   > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                 echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                  echo 20                                   > /sys/module/cpu_boost/parameters/boost_ms
                  echo 1728000                              > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 1497600                              > /sys/module/cpu_boost/parameters/input_boost_freq
@@ -29,11 +29,11 @@ case "$target" in
                  echo 2265600                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                 echo "20000 1400000:40000 1700000:20000"  > /sys/devices/system/cpu/cpufreq/conservative/above_hispeed_delay
-                 echo 90                                   > /sys/devices/system/cpu/cpufreq/conservative/go_hispeed_load
-                 echo 1190400                              > /sys/devices/system/cpu/cpufreq/conservative/hispeed_freq
-                 echo "85 1500000:90 1800000:70"           > /sys/devices/system/cpu/cpufreq/conservative/target_loads
-                 echo 40000                                > /sys/devices/system/cpu/cpufreq/conservative/min_sample_time
+                 echo "20000 1400000:40000 1700000:20000"  > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                 echo 90                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                 echo 1190400                              > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                 echo "85 1500000:90 1800000:70"           > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                 echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                  echo 20                                   > /sys/module/cpu_boost/parameters/boost_ms
                  echo 1728000                              > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 1190400                              > /sys/module/cpu_boost/parameters/input_boost_freq
@@ -47,11 +47,11 @@ case "$target" in
                  echo 1036800                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 1036800                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 1036800                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                 echo "40000"                              > /sys/devices/system/cpu/cpufreq/conservative/above_hispeed_delay
-                 echo 90                                   > /sys/devices/system/cpu/cpufreq/conservative/go_hispeed_load
-                 echo 960000                               > /sys/devices/system/cpu/cpufreq/conservative/hispeed_freq
-                 echo "85 960000:70"                       > /sys/devices/system/cpu/cpufreq/conservative/target_loads
-                 echo 40000                                > /sys/devices/system/cpu/cpufreq/conservative/min_sample_time
+                 echo "40000"                              > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                 echo 90                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                 echo 960000                               > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                 echo "85 960000:70"                       > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                 echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                  echo 0                                    > /sys/module/cpu_boost/parameters/boost_ms
                  echo 960000                               > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 960000                               > /sys/module/cpu_boost/parameters/input_boost_freq
@@ -92,26 +92,26 @@ case "$target" in
                  echo "msm_cpufreq"                        > $dev_governor
               ;;
             "middle")
-                 echo conservative                         > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                 echo conservative                         > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-                 echo conservative                         > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-                 echo conservative                         > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                  echo 2457600                              > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                  echo 2457600                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 2457600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 2457600                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                 echo 40000                            	   > /sys/devices/system/cpu/cpufreq/conservative/above_hispeed_delay
-                 echo 70                                   > /sys/devices/system/cpu/cpufreq/conservative/go_hispeed_load
-                 echo 2457600                              > /sys/devices/system/cpu/cpufreq/conservative/hispeed_freq
-                 echo 70                                   > /sys/devices/system/cpu/cpufreq/conservative/target_loads
-                 echo 40000                                > /sys/devices/system/cpu/cpufreq/conservative/min_sample_time
+                 echo 40000                            > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                 echo 70                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                 echo 2457600                              > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                 echo 70                      > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                 echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                  echo 20                                   > /sys/module/cpu_boost/parameters/boost_ms
                  echo 1958400                              > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 1728000                              > /sys/module/cpu_boost/parameters/input_boost_freq
                  echo 40                                   > /sys/module/cpu_boost/parameters/input_boost_ms
                  echo 255                                  > /sys/class/leds/lcd-backlight/max_brightness
                  echo 578000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
-                 echo msm-adreon-tz                        > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+                 echo msm-adreon-tz                           > /sys/class/kgsl/kgsl-3d0/devfreq/governor
                  echo "msm_cpufreq"                        > $dev_governor
                  start mpdecision
              ;;
