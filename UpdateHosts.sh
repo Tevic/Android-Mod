604ca823b977296860c463e436f24f72eb83a3d7
Tevic <tevic.tt@gmail.com>
2015-10-31 00:26:25 +0800

    Add UpdateHosts Script

--- UpdateHosts.sh
+++ UpdateHosts.sh
@@ -9,11 +9,10 @@ mv hosts 2.txt
 
 sed -n '/^127.0.0.1/p' 1.txt >> 3.txt
 sed -n '/^127.0.0.1/p' 2.txt >> 3.txt
+sed -i '/localhost/d' 3.txt
 
 echo '#' $(date) > 4.txt
 sort 3.txt | uniq >> 4.txt
-
-sed -i '/localhost/d' 4.txt
 
 cp 4.txt hosts
 rm 1.txt 2.txt 3.txt 4.txt
