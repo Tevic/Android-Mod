cb426d6f57d38d510eb6228e2a1f85520a1349ac
TevicTT <tevic.tt@gmail.com>
2016-03-22 00:57:27 +0800

    Update Hosts Script

--- UpdateHosts.sh
+++ UpdateHosts.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-rm 1.txt 2.txt 3.txt 4.txt hosts
+rm *.txt hosts
 URL1=https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt
 URL2=https://raw.githubusercontent.com/vokins/simpleu/master/hosts
-wget --no-check-certificate $URL1
-mv hosts.txt 1.txt
-wget --no-check-certificate $URL2
-mv hosts 2.txt
+URL3=https://raw.githubusercontent.com/lack006/Android-Hosts-L/master/hosts_files/2016_hosts/AD
 
-sed -n '/^127.0.0.1/p' 1.txt >> 3.txt
-sed -n '/^127.0.0.1/p' 2.txt >> 3.txt
-sed -i '/localhost/d' 3.txt
+wget --no-check-certificate $URL1 -O A.txt
+wget --no-check-certificate $URL2 -O B.txt
+wget --no-check-certificate $URL3 -O C.txt
 
-echo '#' $(date) > 4.txt
-sort 3.txt | uniq >> 4.txt
+sed -n '/^127.0.0.1/p' A.txt >> 1.txt
+sed -n '/^127.0.0.1/p' B.txt >> 1.txt
+sed -n '/^127.0.0.1/p' C.txt >> 1.txt
+sed -i '/localhost/d' 1.txt
 
-cp 4.txt hosts
-rm 1.txt 2.txt 3.txt 4.txt
+echo '#' $(date) > 2.txt
+sort 1.txt | uniq >> 2.txt
+
+cp 2.txt hosts
+rm *.txt
