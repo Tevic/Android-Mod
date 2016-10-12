ced2d6c87d19a13d170bc70bcb2dd3e05474a199
TevicTT <tevic.tt@gmail.com>
2016-10-12 10:12:40 +0800

    update hosts

--- UpdateHosts.sh
+++ UpdateHosts.sh
@@ -3,14 +3,17 @@ rm *.txt hosts
 URL1=https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt
 URL2=https://raw.githubusercontent.com/vokins/simpleu/master/hosts
 URL3=https://raw.githubusercontent.com/lack006/Android-Hosts-L/master/hosts_files/2016_hosts/AD
+URL4=https://raw.githubusercontent.com/vokins/yhosts/master/hosts
 
 wget --no-check-certificate $URL1 -O A.txt
 wget --no-check-certificate $URL2 -O B.txt
 wget --no-check-certificate $URL3 -O C.txt
+wget --no-check-certificate $URL4 -O D.txt
 
 sed -n '/^127.0.0.1/p' A.txt >> 1.txt
 sed -n '/^127.0.0.1/p' B.txt >> 1.txt
 sed -n '/^127.0.0.1/p' C.txt >> 1.txt
+sed -n '/^127.0.0.1/p' D.txt >> 1.txt
 sed -i '/localhost/d' 1.txt
 
 echo '#' $(date) > 2.txt
