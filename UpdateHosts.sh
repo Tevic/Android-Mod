d7939569c4eda8397960dc14cdffa228f1d53605
TevicTT <tevic.tt@gmail.com>
2017-05-14 16:40:13 +0800

    update hosts and script

--- UpdateHosts.sh
+++ UpdateHosts.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 rm *.txt hosts
-URL1=https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt
-URL2=https://raw.githubusercontent.com/vokins/simpleu/master/data/hosts
-URL3=https://raw.githubusercontent.com/lack006/Android-Hosts-L/master/hosts_files/2016_hosts/AD
-URL4=https://raw.githubusercontent.com/vokins/yhosts/master/hosts
+URLS=(
+    https://coding.net/u/Elaine_ni/p/hosts/git/raw/master/hosts.txt
+    https://raw.githubusercontent.com/lack006/Android-Hosts-L/master/hosts_files/2017_hosts/AD
+    https://raw.githubusercontent.com/vokins/yhosts/master/hosts
+)
 
-wget --no-check-certificate $URL1 -O A.txt
-wget --no-check-certificate $URL2 -O B.txt
-wget --no-check-certificate $URL3 -O C.txt
-wget --no-check-certificate $URL4 -O D.txt
+INDEX=0
+for URL in ${URLS[@]}; do
+{
+    (( INDEX++ ))
+    echo $INDEX. $URL
+    wget --no-check-certificate $URL -O $INDEX.txt
+    sed -n '/^127.0.0.1/p' $INDEX.txt >> Temp1.txt
+}
+done
+sed -i '/localhost/d' Temp1.txt
 
-sed -n '/^127.0.0.1/p' A.txt >> 1.txt
-sed -n '/^127.0.0.1/p' B.txt >> 1.txt
-sed -n '/^127.0.0.1/p' C.txt >> 1.txt
-sed -n '/^127.0.0.1/p' D.txt >> 1.txt
-sed -i '/localhost/d' 1.txt
+echo '#' $(date) > Temp2.txt
+sort Temp1.txt | uniq >> Temp2.txt
 
-echo '#' $(date) > 2.txt
-sort 1.txt | uniq >> 2.txt
-
-cp 2.txt hosts
+cp Temp2.txt hosts
 rm *.txt
